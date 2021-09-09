9f3a04b98eb6e701efcd6208a50e7d9c0350fce7
htetaungkyaw <htetaung200071@gmail.com>
2021-09-09 12:45:47 +0630

    add new final postgres

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ now = datetime.datetime.now()
 date = now.strftime("%d %B %Y")
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',"sqlite:///items.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 db = SQLAlchemy(app)
