bfdf5c2d1a65bdc47481b519048bd269f788c3b9
htetaungkyaw <htetaung200071@gmail.com>
2021-09-09 12:17:35 +0630

    change code

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ now = datetime.datetime.now()
 date = now.strftime("%d %B %Y")
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////portfolio projects/an online shop/item.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio projects/an online shop/item.db'
 app.config['SECRET_KEY'] = 'adadiadjadhydjqddq27e2jdhUDA'
 db = SQLAlchemy(app)
 login_manager = LoginManager()
